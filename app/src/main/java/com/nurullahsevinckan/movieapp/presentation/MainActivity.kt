c1b6112c750e1078bb67f4df1b00d07b31470cbd
SNurullahS <nurullah.sevinckan91@gmail.com>
2025-02-23 18:30:50 +0300

    Navigation for screens implemented

--- app/src/main/java/com/nurullahsevinckan/movieapp/presentation/MainActivity.kt
+++ app/src/main/java/com/nurullahsevinckan/movieapp/presentation/MainActivity.kt
@@ -6,11 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.nurullahsevinckan.movieapp.presentation.ui.theme.MovieAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,29 +26,27 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MovieAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background){
+
+                    val navController  = rememberNavController()
+                    NavHost(navController = navController, startDestination = Screen.MovieScreen.route){
+
+                        //Movie screen
+                        composable(route = Screen.MovieScreen.route){
+
+                        }
+
+
+                        //Movie details screen
+                        composable(route = Screen.MovieDetailScreen.route){
+
+
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MovieAppTheme {
-        Greeting("Android")
     }
 }

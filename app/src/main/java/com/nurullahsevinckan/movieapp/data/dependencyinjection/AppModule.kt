c345f024b36564f0658d0aaf2f2350ef30d38d15
SNurullahS <nurullah.sevinckan91@gmail.com>
2025-02-28 00:02:35 +0300

    Hilt local storage (roomdb) providers added

--- app/src/main/java/com/nurullahsevinckan/movieapp/data/dependencyinjection/AppModule.kt
+++ app/src/main/java/com/nurullahsevinckan/movieapp/data/dependencyinjection/AppModule.kt
@@ -1,12 +1,19 @@
 package com.nurullahsevinckan.movieapp.data.dependencyinjection
 
+import android.content.Context
+import androidx.room.Room
+import com.nurullahsevinckan.movieapp.data.local.dao.FavoriteMovieDao
+import com.nurullahsevinckan.movieapp.data.local.database.MovieDatabase
 import com.nurullahsevinckan.movieapp.data.remote.MovieAPI
+import com.nurullahsevinckan.movieapp.data.repository.FavoriteMovieRepositoryImpl
 import com.nurullahsevinckan.movieapp.data.repository.MovieRepositoryImpl
+import com.nurullahsevinckan.movieapp.domain.repository.FavoriteMovieRepository
 import com.nurullahsevinckan.movieapp.domain.repository.MovieRepository
 import com.nurullahsevinckan.movieapp.util.Constants.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -31,5 +38,26 @@ object AppModule {
     @Singleton
     fun provideMovieRepository(api : MovieAPI) : MovieRepository{
         return MovieRepositoryImpl(api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieDatabase(@ApplicationContext context: Context): MovieDatabase {
+        return Room.databaseBuilder(
+            context,
+            MovieDatabase::class.java,
+            "movie_database"
+        ).build()
+    }
+
+    @Provides
+    fun provideFavoriteMovieDao(database: MovieDatabase): FavoriteMovieDao {
+        return database.favoriteMovieDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideFavoriteMovieRepository(dao: FavoriteMovieDao): FavoriteMovieRepository {
+        return FavoriteMovieRepositoryImpl(dao)
     }
 }

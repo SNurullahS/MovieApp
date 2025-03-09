030540f7a3ed6272a237b20a1d9134f645d0900a
SNurullahS <nurullah.sevinckan91@gmail.com>
2025-03-09 20:46:29 +0300

    Login screen improvement * Dynamic size of movie poster

--- app/src/main/java/com/nurullahsevinckan/movieapp/presentation/movie_detail/views/MovieDetailScreenImage.kt
+++ app/src/main/java/com/nurullahsevinckan/movieapp/presentation/movie_detail/views/MovieDetailScreenImage.kt
@@ -2,6 +2,8 @@ package com.nurullahsevinckan.movieapp.presentation.movie_detail.views
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
@@ -9,21 +11,25 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.nurullahsevinckan.movieapp.domain.model.MovieDetail
-
 @Composable
 fun ImageDetailComp(movie: MovieDetail) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth()
+    ) {
         Image(
             painter = rememberAsyncImagePainter(model = movie.Poster),
             contentDescription = movie.Title,
+            contentScale = ContentScale.Crop, // Resmin alanı doldururken oranı korur
             modifier = Modifier
                 .padding(16.dp)
-                .size(width = 500.dp, height = 500.dp)
+                .fillMaxWidth()   // Ekranın genişliğini doldurur
+                .aspectRatio(0.75f)  // Yükseklik, genişliğin 1 katı olur
                 .clip(RectangleShape)
         )
     }
 }
-
